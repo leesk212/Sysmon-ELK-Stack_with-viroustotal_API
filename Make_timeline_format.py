13bcf84c45994d8758d62c91486c8c2309e986d9
leesk212@gmail.com <leesk212@gmail.com>
2020-08-20 18:18:09 +0900

    change position
    Signed-off-by: leesk212@gmail.com <leesk212@gmail.com>

--- Make_timeline_format.py
+++ Make_timeline_format.py
@@ -2,8 +2,8 @@ def from_utctime(utctime):
     #from: '2020-08-18 07:23:34.837'
     #to:   '2020-08-18T07:23:34.839Z'
 
-    first,second = map(str,utctime.split())
-    first=first+"T"
-    second=second+"Z"
+    first, second = map(str, utctime.split())
+    first = first+"T"
+    second = second+"Z"
     timeline = first+second
     return timeline
