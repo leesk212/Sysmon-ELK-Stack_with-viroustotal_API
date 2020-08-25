fb30a6ed770f4537173270a9d07d4efa97afe58b
leesk212@gmail.com <leesk212@gmail.com>
2020-08-25 16:53:15 +0900

    20/08/25 release Sysmon - EL - PyQT5 _version_1
    Signed-off-by: leesk212@gmail.com <leesk212@gmail.com>

--- Code/SeokMin_ui.py
+++ Code/SeokMin_ui.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import json
 
-form_class = uic.loadUiType('./code/SeokMin.ui')[0]
+form_class = uic.loadUiType('./Code/SeokMin.ui')[0]
 
 class Mywindow(QMainWindow, form_class):
     def __init__(self, rs):
